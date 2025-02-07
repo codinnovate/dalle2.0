ce3c1e3cc0690b6eca129719674569d3220749eb
samuel <adeyemis710@gmail.com>
2025-02-07 21:40:53 +0100

    started working on page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,14 @@
-
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
+
+
+const RenderCards = ({data}){
+  if(data)
+}
 export default function Home() {
+  const [loading, setLoading] = useState()
   return (
     <div>
       Hello World!
