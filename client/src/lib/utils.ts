74322f7ba4ef20f7731d756bf03e0221d2f58de5
samuel <adeyemis710@gmail.com>
2025-02-09 22:27:21 +0100

    first commit

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -2,6 +2,8 @@ import { surpriseMePrompts } from "@/constants";
 
 export function getRandomPrompts(prompt: string){
     const randomPromptIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+
+    
     const randomPrompt  = surpriseMePrompts[randomPromptIndex];
     if(randomPrompt === prompt) return getRandomPrompts(prompt);
     return randomPrompt;
