b3cfe632bbaa56a3df0d5da2c7ba630730c5a169
samuel <adeyemis710@gmail.com>
2025-02-11 20:08:49 +0100

    create post

--- server/routes/post-routes.js
+++ server/routes/post-routes.js
@@ -6,5 +6,13 @@ export const createPost = async (req, res) => {
     const { title, content, image } = req.body;
     const result = await cloudinary.uploader.upload(image);
     const { secure_url } = result;
+    const newPost = new Post({
+        title,
+        content,
+        photo: secure_url
+    });
+    await newPost.save();
+    res.status(201).json(newPost);
     
+
 }
